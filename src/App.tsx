6f9d9b742b1c57f6ffd17191402f2bf02a22e0d9
Sean Khan <seanny813@optonline.net>
2024-03-03 18:13:38 -0500

    Managing Props & State

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import TodoList from './components/TodoList';
-import NewTodo from './components/NewTodo';
+import React, { useState } from "react";
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
 
 function App() {
-    const todos = [{ id: 't1', text: 'Finish the course'}];
+	const [todos, setTodos] = useState<Todo[]>([]);
 
-    const todoAddHandler = (text:string) => {
-        console.log(text);
-    };
+	const todoAddHandler = (text: string) => {
+		setTodos((prevTodos) => [
+			...prevTodos,
+			{ id: Math.random().toString(), text: text },
+		]);
+	};
 
-  return (
-    <div className="App">
-      <NewTodo onAddTodo={todoAddHandler}/>
-      <TodoList items={todos} />
-    </div>
-  );
-};
+	const todoDeleteHandler = (todoId: string) => {
+		setTodos((prevTodos) => {
+			return prevTodos.filter((todo) => todo.id !== todoId);
+		});
+	};
+
+	return (
+		<div className="App">
+			<NewTodo onAddTodo={todoAddHandler} />
+			<TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
+		</div>
+	);
+}
 
 export default App;
